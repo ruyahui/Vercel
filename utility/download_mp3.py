f70bf08bfcf3c091e9dfac07ff2e20030bc782f6
ruyahui <ruyahui@gmail.com>
2023-02-17 17:35:12 +0800

    debug youtube51

--- utility/download_mp3.py
+++ utility/download_mp3.py
@@ -90,7 +90,7 @@ def get_playlist(url):
 	audio_wave_file = audio_file.replace(".mp3",".wav")
 	#if not os.path.isfile(audio_wave_file):
 	#	mp3_to_wave(audio_file)
-	return [yt.title],audio_wave_file
+	return [yt.title],audio_file
 
 '''
 url='https://www.youtube.com/playlist?list=PLOB7G19x6JpPcNiPj7llUNQPrtbbcpVVN'
